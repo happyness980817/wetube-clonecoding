5b0540000a0a4f0076c86c8c6ffb04e0f92c90b7
hoegiclass <happyness980817@gmail.com>
2025-02-27 21:22:13 +0900

    async await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,45 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+/* 
+console.log("start");
+Video.find({}, (error, videos) => {
+  if(error){
+    return res.render("server-error");
+  };
+  return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("finished");  --> start, finished, videos 순서로 출력
+*/
+
+export const home = async (req, res) => {
+  console.log("start");
+  const videos = await Video.find({}); // 데이터베이스에서 데이터를 읽어올 때까지 기다린다.
+  console.log(videos);
+  console.log("finished");
+  return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   return res.render("edit", { pageTitle: `Editing` });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { home } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
