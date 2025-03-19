6143e76b9ea0517c9195f6fb926410e726a34998
hoegiclass <happyness980817@gmail.com>
2025-03-19 17:56:07 +0900

    edit profile POST (should change postEdit to check if updated data equals existing ones)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo);
 // videoRouter.get("/upload", getUpload);
 // videoRouter.post("/upload", postUpload);
-videoRouter.route("/upload").get(getUpload).post(postUpload); // 위에 두줄 쓰는거랑 동일기능
+videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(postUpload); // 위에 두줄 쓰는거랑 동일기능
 
 export default videoRouter;
